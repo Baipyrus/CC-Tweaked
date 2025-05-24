5960568d98c759dc600cb28bf760cde633d5072f
Baipyrus <simon.waltemathe@gmail.com>
2025-05-24 21:46:07 +0200

    chore: format, optimize and comment existing script

--- readPeripherals.lua
+++ readPeripherals.lua
@@ -1,47 +1,63 @@
--- Reference (at 24.05.2025): https://pastebin.com/uigZcEsw
-
 term.clear()
+-- Base height of built-in computer display
+-- TODO: Detect display/monitor width?
+local linesPerPage = 14
+
 local sides = peripheral.getNames()
-local linesPerPage = 16  -- Depending on monitor size
 local allLines = {}
 
+-- Convert all peripheral methods to
 for _, side in ipairs(sides) do
-  local pType = peripheral.getType(side)
-  table.insert(allLines, "Peripheral: " .. side .. " (" .. pType .. ")")
+	-- Set peripheral headers
+	local pType = peripheral.getType(side)
+	table.insert(allLines, "Side: " .. side)
+	table.insert(allLines, "Type: " .. pType)
+	table.insert(allLines, "")
 
-  local methods = peripheral.getMethods(side)
-  if methods then
-    for _, m in ipairs(methods) do
-      table.insert(allLines, "  - " .. m)
-    end
-  else
-    table.insert(allLines, "  (no methods found)")
-  end
+	local methods = peripheral.getMethods(side)
+	if methods == nil then
+		table.insert(allLines, " (No methods found!)")
+		goto continue
+	end
+
+	-- Save peripheral methods in table
+	for _, m in ipairs(methods) do
+		table.insert(allLines, " - " .. m)
+	end
+
+	::continue::
 end
 
+-- Main loop/logic
 local currentPage = 1
 local totalPages = math.ceil(#allLines / linesPerPage)
 
 while true do
-  term.clear()
-  term.setCursorPos(1, 1)
-  print("Peripherals (Page " .. currentPage .. "/" .. totalPages .. ")")
-  print("--------------------------------")
+	term.clear()
+	term.setCursorPos(1, 1)
 
-  local startLine = (currentPage - 1) * linesPerPage + 1
-  local endLine = math.min(startLine + linesPerPage - 1, #allLines)
-  for i = startLine, endLine do
-    print(allLines[i])
-  end
+	-- Print global headers
+	print("Peripherals (Page " .. currentPage .. "/" .. totalPages .. ")")
+	print("--------------------------------")
 
-  print("\n[N] Next | [P] Previous | [Q] Quit")
-  local event, key = os.pullEvent("key")
+	-- Print current page content
+	-- TODO: Page scrolling can break due to line wrap
+	local startLine = (currentPage - 1) * linesPerPage + 1
+	local endLine = math.min(startLine + linesPerPage - 1, #allLines)
+	print(table.concat(allLines, "\n", startLine, endLine))
 
-  if key == keys.q then
-    break
-  elseif key == keys.n and currentPage < totalPages then
-    currentPage = currentPage + 1
-  elseif key == keys.p and currentPage > 1 then
-    currentPage = currentPage - 1
-  end
+	-- Print global footers
+	print("\n[N] Next | [P] Previous | [Q] Quit")
+
+	-- Event handler: scroll pages or quit
+	local _, key = os.pullEvent("key")
+	if key == keys.q then
+		term.clear()
+		term.setCursorPos(1, 1)
+		break
+	elseif key == keys.n and currentPage < totalPages then
+		currentPage = currentPage + 1
+	elseif key == keys.p and currentPage > 1 then
+		currentPage = currentPage - 1
+	end
 end
