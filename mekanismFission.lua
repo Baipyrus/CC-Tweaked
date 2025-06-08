b3ef44b51e001bf4c56823c94435737bd8b5d1c4
Baipyrus <simon.waltemathe@gmail.com>
2025-06-08 17:26:55 +0200

    feat: implement mekanism fission reactor logic

--- mekanismFission.lua
+++ mekanismFission.lua
@@ -49,12 +49,46 @@ local display_coroutine = coroutine.create(function()
 	pd_main.display(true)
 	exit = true
 end)
+
+local reactor_coroutine = coroutine.create(function()
+	-- Keeping track of the scram time of reactor
+	local deactivated = 0
+
+	while not exit do
+		local isActive = reactor.getStatus()
+
+		-- Reactor scram conditions
+		local damaged = reactor.getDamagePercent() > 100
+		local overheated = reactor.getTemperature() > 1200
+		local wasteFilled = reactor.getWasteFilledPercentage() > 95
+
+		-- Cool-off period for reactor to recover during
+		local diffTime = os.difftime(os.time(), deactivated)
+
+		if isActive and (damaged or overheated or wasteFilled) then
+			reactor.scram()
+			deactivated = os.time()
+		elseif not isActive and diffTime >= 60 then
+			reactor.activate()
+		end
+
+		-- Yield coroutine for parallelized logic
+		coroutine.yield()
+	end
+end)
+
 -- Main event loop
 while not exit do
 	local display_dead = coroutine.status(display_coroutine) == "dead"
+	local reactor_dead = coroutine.status(reactor_coroutine) == "dead"
 
 	-- Resume display, if running
 	if not display_dead then
 		coroutine.resume(display_coroutine)
+	end
+
+	-- Resume reactor logic, if running
+	if not reactor_dead then
+		coroutine.resume(reactor_coroutine)
 	end
 end
